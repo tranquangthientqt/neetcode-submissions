029d2e3707b28cb1c719d07c0a7228beec7d0745
Tran Quang Thien <tranquangthientqt1@gmail.com>
2026-05-08 23:39:45 +0700

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "thien"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
