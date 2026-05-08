0c2cf1b9f78fdfbef7dff8da3f16ad104829c54f
Tran Quang Thien <tranquangthientqt1@gmail.com>
2026-05-08 23:36:52 +0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 10
+float_type = 3.14
+bool_type = True
+string_type = "thien"
+list_type = [1,2,3]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
