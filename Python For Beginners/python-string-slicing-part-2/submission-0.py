b962c7e59d7e999c225316fb2ce641306dc763d9
Tran Quang Thien <tranquangthientqt1@gmail.com>
2026-05-09 23:06:57 +0700

    Add: python-string-slicing-part-2 - submission-0

---
+++ Python For Beginners/python-string-slicing-part-2/submission-0.py
@@ -0,0 +1,17 @@
+def first_n_characters(s: str, n: int) -> str:
+    if len(s) >= n:
+        return s[:n]
+
+def last_n_characters(s: str, n: int) -> str:
+    if len(s) >= n:
+        return s[len(s) - n:]
+
+
+# do not modify below this line
+print(first_n_characters("NeetCode", 3))
+print(first_n_characters("NeetCode", 4))
+print(first_n_characters("NeetCode", 8))
+
+print(last_n_characters("NeetCode", 3))
+print(last_n_characters("NeetCode", 4))
+print(last_n_characters("NeetCode", 8))
