27d31bb66790894d3a13e87dde6dda9c781287d3
Tran Quang Thien <tranquangthientqt1@gmail.com>
2026-05-10 01:06:09 +0700

    Add: python-strings-are-immutable - submission-1

---
+++ Python For Beginners/python-strings-are-immutable/submission-1.py
@@ -0,0 +1,12 @@
+def remove_fourth_character(word: str) -> str:
+    if len(word) <= 4:
+        return word
+    before_second = word[:3]
+    after_second = word[4:]
+    return before_second + after_second
+
+    
+
+# do not modify below this line
+print(remove_fourth_character("NeetCode"))
+print(remove_fourth_character("Hello"))
